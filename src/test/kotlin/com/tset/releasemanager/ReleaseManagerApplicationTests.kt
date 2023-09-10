c675a754eaa3be5c8e614c4de558e81056ded3e1
scibaric <sebastijan.cibaric@gmail.com>
2023-09-10 07:25:45 +0200

    Initial commit

---
+++ src/test/kotlin/com/tset/releasemanager/ReleaseManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tset.releasemanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReleaseManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
